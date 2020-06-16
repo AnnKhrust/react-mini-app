cb30c0cf2ab8bd3ded4fb85429b9338d0724f207
AnnKhrust <anna.khrust@gmail.com>
2020-06-16 16:59:14 +0300

    update project

---
+++ src/game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    board</div>
+                <div className="game-info"></div>
+                <div>{'status'}</div>
+                <ul>{/**/}</ul>
+            </div>
+        )
+    }
+}
